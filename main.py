672f3acc8ad4665603d16607d2d172cd83b19be3
arya-raditya <aryaraditya.kusuma2006@gmail.com>
2026-02-05 08:34:01 +0700

    branch subtract

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Calculator: 
-    def add(self, a, b): 
-        return a + b 
-    def subtract(self, a, b): 
-        return a - b 
-    def multiply(self, a, b): 
-        return a * b 
-    def divide(self, a, b): 
-        return a / b 
-    def modulo(self, a, b): 
-        return a % b 
-    def power(self, a, b): 
-        return a ** b 
+    def add(self, a_subtract, b_subtract): 
+        return a_subtract + b_subtract 
+    def subtract(self, a_subtract, b_subtract): 
+        return a_subtract - b_subtract 
+    def multiply(self, a_subtract, b_subtract): 
+        return a_subtract * b_subtract 
+    def divide(self, a_subtract, b_subtract): 
+        return a_subtract / b_subtract 
+    def modulo(self, a_subtract, b_subtract): 
+        return a_subtract % b_subtract 
+    def power(self, a_subtract, b_subtract): 
+        return a_subtract ** b_subtract 
     
 if __name__ == "__main__": 
     calc = Calculator() 
