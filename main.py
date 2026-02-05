97d7851251ebc95a99bc20e71ccbd2502d4c8bc1
vissutagunawan <vglim3653@gmail.com>
2026-02-05 08:29:33 +0700

    ciki

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Calculator: 
-    def add(self, a, b): 
-        return a + b 
-    def subtract(self, a, b): 
-        return a - b 
-    def multiply(self, a, b): 
-        return a * b 
-    def divide(self, a, b): 
-        return a / b 
-    def modulo(self, a, b): 
-        return a % b 
-    def power(self, a, b): 
-        return a ** b 
+    def add(self, ciki, b): 
+        return ciki + b 
+    def subtract(self, ciki, b): 
+        return ciki - b 
+    def multiply(self, ciki, b): 
+        return ciki * b 
+    def divide(self, ciki, b): 
+        return ciki / b 
+    def modulo(self, ciki, b): 
+        return ciki % b 
+    def power(self, ciki, b): 
+        return ciki ** b 
     
 if __name__ == "__main__": 
     calc = Calculator() 
