40b481c56697bb4e75027b0152f6925c0ddf6d1b
karolinajocelyn <karolina.jocelyn@gmail.com>
2026-02-05 08:33:17 +0700

    fix variables

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Calculator: 
-    def add(self, a, b): 
-        return a + b 
-    def subtract(self, a, b): 
-        return a - b 
-    def multiply(self, a, b): 
-        return a * b 
-    def divide(self, a, b): 
-        return a / b 
-    def modulo(self, a, b): 
-        return a % b 
-    def power(self, a, b): 
-        return a ** b 
+    def add(self, c, d): 
+        return c + d 
+    def subtract(self, c, d): 
+        return c - d
+    def multiply(self, c, d): 
+        return c * d 
+    def divide(self, c, d): 
+        return c / d 
+    def modulo(self, c, d): 
+        return c % d 
+    def power(self, c, d): 
+        return c ** d 
     
 if __name__ == "__main__": 
     calc = Calculator() 
